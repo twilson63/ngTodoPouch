65a0b7eb1e21d1ed17430ad3a022345735724533
Tom Wilson <tom@jackhq.com>
2014-05-09 21:31:13 -0400

    pull out and minify 3rdparty libs

--- app/index.js
+++ app/index.js
@@ -1,6 +1,3 @@
-require('angular/angular');
-require('angular-ui-router/release/angular-ui-router');
-
 angular.module('TodoApp', ['ui.router',
   require('./splash').name,
   require('./account').name,
@@ -10,7 +7,32 @@ angular.module('TodoApp', ['ui.router',
 .controller('ApplicationCtrl', require('./app-controller'))
 .factory('$db', function() {
   //var url = $window.location.origin + '/db/' + $window.localStorage.getItem('user');
-  return require('pouchdb')('myTodos');
+  return PouchDb('myTodos');
+})
+.constant('$us', require('underscore'))
+.factory('$origin', function($window) {
+  return $window.location.origin;
+})
+.factory('$set', function($window) {
+  return function (key, value) {
+    $window.localStorage.setItem(key, value);
+  };
+})
+.factory('$user', function ($window) {
+  return $window.localStorage.getItem('user');
+})
+
+;
+angular.module('TodoApp', ['ui.router',
+  require('./splash').name,
+  require('./account').name,
+  require('./lists').name
+])
+.config(require('./app-config'))
+.controller('ApplicationCtrl', require('./app-controller'))
+.factory('$db', function() {
+  //var url = $window.location.origin + '/db/' + $window.localStorage.getItem('user');
+  return PouchDB('myTodos');
 })
 .constant('$us', require('underscore'))
 .factory('$origin', function($window) {
