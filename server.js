363ebc5e57ce8f75c64b62991312ac82a62bb654
Tom Wilson <tom@jackhq.com>
2014-05-27 19:00:54 -0400

    change port to env

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var app = require(__dirname + '/app');
 //console.log(JSON.stringify(process.env));
-var port = process.argv[2] || 3000;
+var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('listening to port ' + port);
